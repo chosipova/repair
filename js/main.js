7c49dcc96e6591d759a6d42a93807b76682eab8a
Christina Osipova <chosipova@gmail.com>
2019-02-28 22:47:44 +0300

    правки по модальному окну

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close')
+var timerId;
 
 button.addEventListener('click', function () {
   modal.classList.add('modal_active');
-  setTimeout(function () {
+  timerId = setTimeout(function () {
     modal.classList.remove('modal_active')
   }, 5000);
+  console.log(timerId);
 });
 
 close.addEventListener('click', function () {
-  modal.classList.remove('modal_active')
+  console.log(timerId);
+  clearTimeout(timerId);
+  modal.classList.remove('modal_active');
 });
