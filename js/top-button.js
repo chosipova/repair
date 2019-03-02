18852b9d53dbd102f69407c68eaae9bc970fec19
Christina Osipova <chosipova@gmail.com>
2019-03-02 18:45:55 +0300

    кнопка вверх jquery

---
+++ js/top-button.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  var btn = $('#top-button');
+  console.log(btn);
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('top-button_show');
+      console.log("Здесь");
+    } else {
+      btn.removeClass('top-button_show');
+      console.log("Здесь");
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
+
+});
