ce91d53c30fed18bf9e1f6260634634c21ca4e6b
Christina Osipova <chosipova@gmail.com>
2019-03-06 20:03:52 +0300

    сложный 20 урок, анимация и скрипт

---
+++ js/scroll-animation.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var elem = $('.step');
+
+  $(window).scroll(function () {
+    // console.log($(window).scrollTop());
+    if ($(window).scrollTop() > elem.offset().top - $(window).height()) {
+      console.log("я здесь");
+      elem.addClass('step-anim');
+    } else {
+      elem.removeClass('step-anim');
+    }
+  });
+});
